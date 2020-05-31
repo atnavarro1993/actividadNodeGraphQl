8c12a63468ff65857a51f2eb993037bd0acd3c12
Ariel <a.t.navarro.1993@gmail.com>
2020-05-31 00:42:47 -0300

    nodeGraphQl

--- schema/query.js
+++ schema/query.js
@@ -1,36 +1,61 @@
-const {
-    GraphQLObjectType,
-    GraphQLList,
-    GraphQLInt
-} = require('graphql');
-const {
-    CourseType,
-    StudentType,
-    GradeType} = require('./structure/structure');
-const students = require('./data/student.json');
-const courses = require('./data/course.json');
-const grades = require('./data/grade.json');
+const { GraphQLObjectType, GraphQLList, GraphQLInt } = require("graphql");
+const { CourseType, StudentType, GradeType } = require("./structure/structure");
+const students = require("./data/student.json");
+const courses = require("./data/course.json");
+const grades = require("./data/grade.json");
 
 const RootQueryType = new GraphQLObjectType({
-    name: "Query",
-    description:"Root Query",
-    fields:()=>({
-        students: {
-            type: new GraphQLList(StudentType),
-            description:"list of all students",
-            resolve:()=>students
+  name: "Query",
+  description: "Root Query",
+  fields: () => ({
+    students: {
+      type: new GraphQLList(StudentType),
+      description: "list of all students",
+      resolve: () => students,
+    },
+    courses: {
+      type: new GraphQLList(CourseType),
+      description: "list of all courses",
+      resolve: () => courses,
+    },
+    grades: {
+      type: new GraphQLList(GradeType),
+      description: "list of all grades",
+      resolve: () => grades,
+    },
+    student: {
+      type: StudentType,
+      description: "shows particular student by id",
+      args: {
+        id: {
+          type: GraphQLInt,
         },
-        courses:{
-            type: new GraphQLList(CourseType),
-            description:"list of all courses",
-            resolve:()=>courses
+      },
+      resolve: (parent, args) =>
+        students.find((student) => student.id === args.id),
+    },
+    course: {
+      type: CourseType,
+      description: "shows particular courses by id",
+      args: {
+        id: {
+          type: GraphQLInt,
         },
-        grades:{
-            type:new GraphQLList(GradeType),
-            description:"list of all grades",
-            resolve:()=>grades
-        }
-    })
+      },
+      resolve: (parent, args) =>
+        courses.find((course) => course.id === args.id),
+    },
+    grade: {
+      type: GradeType,
+      description: "shows particular makrs by id",
+      args: {
+        id: {
+          type: GraphQLInt,
+        },
+      },
+      resolve: (parent, args) => grades.find((grade) => grade.id === args.id),
+    },
+  }),
 });
 
 module.exports = RootQueryType;
