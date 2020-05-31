e27998eb1276bed3d8bfbd10c5e962b0ba1d0b36
Ariel <a.t.navarro.1993@gmail.com>
2020-05-30 23:04:07 -0300

    nodeGraphQl

--- schema/structure/structure.js
+++ schema/structure/structure.js
@@ -3,16 +3,26 @@ const {
     GraphQLString,
     GraphQLInt,
     GraphQLNonNull,
+    GraphQLList
   } = require("graphql");
+  const students = require('../data/student.json');
+  const courses = require('../data/course.json');
+  const grades = require('../data/grade.json');
+  
 
   const CourseType = new GraphQLObjectType({
-    
     name:"courses",
     description:"represent students",
     fields:()=>({
         id:{type:GraphQLNonNull(GraphQLInt)},
         name:{type:GraphQLNonNull(GraphQLString)},
-        description:{type:GraphQLNonNull(GraphQLString)}
+        description:{type:GraphQLNonNull(GraphQLString)},
+        students:{
+            type:new GraphQLList(StudentType),
+            resolve:(course)=>{
+                return students.filter(student=>student.courseId===course.id)
+            }
+        }
     })
 });
 
@@ -23,7 +33,19 @@ const StudentType =  new GraphQLObjectType({
         id:{type:GraphQLNonNull(GraphQLInt)},
         name: { type:GraphQLNonNull(GraphQLString)},
         lastName: {type: GraphQLNonNull(GraphQLString)},
-        courseId: {type:GraphQLNonNull(GraphQLInt)}
+        courseId: {type:GraphQLNonNull(GraphQLInt)},
+        course:{
+            type:CourseType,
+            resolve:(student)=>{
+                return courses.find(course=>course.id===student.courseId)
+            }
+        },
+        grade:{
+            type:GradeType,
+            resolve:(student)=>{
+                return grades.find(grade=>grade.studentId===student.id)
+            }
+        }
     })
 });
 
